65c20624a31e81d1eb10a7d3fee01cd8b3466596
Armourking-12 <vm1304882@gmail.com>
2026-06-22 07:06:33 +0530

    Update robots.ts base url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://helpjuniors.com";
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://helpjuniours.store";
 
   return {
     rules: {
